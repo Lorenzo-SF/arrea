4b1d99b44de72b4a94b4bba5ffa2df7d43f8084d
Mavis <Mavis@users.noreply.github.com>
2026-06-26 22:46:01 +0000

    chore(arrea): bump alaja to v0.3.8 (pote v0.3.0 tagged)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Arrea.MixProject do
   def project do
     [
       app: :arrea,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
