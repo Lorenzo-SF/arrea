5777c1a3b68167de9e2da603cf3029b6209fc7da
Lorenzo Sánchez Fraile <sanchezfrailelorenzo@gmail.com>
2026-07-05 15:39:31 +0200

    update alaja version

--- mix.exs
+++ mix.exs
@@ -99,7 +99,7 @@ defmodule Arrea.MixProject do
 
   defp deps do
     [
-      {:alaja, github: "Lorenzo-SF/alaja", branch: "main"},
+      {:alaja, "~> 2.0.0"},
       {:jason, "~> 1.4"},
       {:telemetry, "~> 1.3"},
       {:telemetry_metrics, "~> 1.1"},
