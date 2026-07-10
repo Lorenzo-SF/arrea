0a9e5196ef7fa4bfb1ad94b058ed44d4041c03f7
Lorenzo Sánchez Fraile <sanchezfrailelorenzo@gmail.com>
2026-07-10 16:28:03 +0200

    chore: update versions

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Arrea.MixProject do
   def project do
     [
       app: :arrea,
-      version: "2.1.0",
+      version: "2.2.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -99,7 +99,7 @@ defmodule Arrea.MixProject do
 
   defp deps do
     [
-      {:alaja, path: "../alaja"},
+      {:alaja, "~> 2.3.0"},
       {:jason, "~> 1.4"},
       {:telemetry, "~> 1.3"},
       {:telemetry_metrics, "~> 1.1"},
